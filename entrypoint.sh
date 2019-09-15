46b1554040894a34e8157648e0fceaa5608f3a94
Markus Staab <maggus.staab@googlemail.com>
2019-09-15 07:55:38 +0200

    skip if not PR comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+set -e
+
+NEUTRAL_EXIT_CODE=0
+
 sh -c "/usr/local/bin/php-cs-fixer fix $*"
 
 (jq -r ".comment.body" "$GITHUB_EVENT_PATH" | grep -Fq "/php-cs-fixer")
@@ -11,6 +15,10 @@ if [ "$COMMENT_KEYWORD_FOUND" -eq 0 ]; then
   	exit 1
   fi
   
+  # skip if not a PR
+  echo "Checking if issue is a pull request..."
+  (jq -r ".issue.pull_request.url" "$GITHUB_EVENT_PATH") || exit $NEUTRAL_EXIT_CODE
+
   REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
   
   git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
